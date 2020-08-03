173fe5f953a7cffef4468b8b1a3a6fd197ba92eb
vs4vijay <vs4vijay@gmail.com>
2020-08-03 17:57:35 +0530

    Add bootstrap

--- installer.sh
+++ installer.sh
@@ -31,8 +31,23 @@ sudo apt-get install silversearcher-ag htop ranger
 ## Install Programming Tools
 sudo apt-get install python python-pip python3 python3-pip
 
-## Installing Tools
-# sudo apt-get install firefox firefox-dev-edition chromium vlc
+## Installing Softwares
+sudo apt-get install firefox firefox-dev-edition chromium vlc
+
+## Installing Editors (Sublime, VSCode)
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+
+wget -qO - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code
+
+
 
 # [ "${SHELL##/*/}" != "zsh" ] && echo "You might need to change default shell to zsh: `chsh -s /bin/zsh`"
 
