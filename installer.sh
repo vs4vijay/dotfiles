0eca2b9d471c8ba2e7316be0453c42de58327231
vs4vijay <vs4vijay@gmail.com>
2020-08-03 18:09:02 +0530

    Add ZSH Plugins

--- installer.sh
+++ installer.sh
@@ -23,18 +23,28 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O install-fzf.sh
 bash install-fzf.sh
 
+## Zsh Plugins
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions 		 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
 
 ## Install Utilities
 sudo apt-get install silversearcher-ag htop ranger
 
+wget https://github.com/Peltoche/lsd/releases/download/0.17.0/lsd_0.17.0_amd64.deb -O lsd.deb
+sudo dpkg -i lsd.deb
+rm -rf lsd.deb
 
 ## Install Programming Tools
 sudo apt-get install python python-pip python3 python3-pip
 
-## Installing Softwares
+
+## Install Softwares
 sudo apt-get install firefox firefox-dev-edition chromium vlc
 
-## Installing Editors (Sublime, VSCode)
+
+## InstallEditors (Sublime, VSCode)
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
